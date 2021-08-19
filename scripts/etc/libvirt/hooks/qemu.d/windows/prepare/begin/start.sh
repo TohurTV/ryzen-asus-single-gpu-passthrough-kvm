4ef9e59ad6796ef109dc1a2626ea4ad6a78c2076
Joshua Webb <joshwebb84@outlook.com>
2021-08-18 20:35:57 -0600

    Changes

--- scripts/etc/libvirt/hooks/qemu.d/windows/prepare/begin/start.sh
+++ scripts/etc/libvirt/hooks/qemu.d/windows/prepare/begin/start.sh
@@ -14,12 +14,14 @@ systemctl set-property --runtime -- user.slice AllowedCPUs=0,6
 systemctl set-property --runtime -- system.slice AllowedCPUs=0,6
 systemctl set-property --runtime -- init.scope AllowedCPUs=0,6
 
+cpupower frequency-set -g performance
+
 modprobe -r amdgpu
-#modprobe -r gpu_sched
-#modprobe -r ttm
-#modprobe -r drm_kms_helper
-#modprobe -r i2c_algo_bit
-#modprobe -r drm
+modprobe -r gpu_sched
+modprobe -r ttm
+modprobe -r drm_kms_helper
+modprobe -r i2c_algo_bit
+modprobe -r drm
 modprobe -r snd_hda_intel
 
 # unbind gpu
@@ -28,3 +30,7 @@ virsh nodedev-detach $VIRSH_GPU_AUDIO
 
 #rtcwake -m mem -s 5
 
+# load vfio
+modprobe vfio
+modprobe vfio_pci
+modprobe vfio_iommu_type1
--- scripts/etc/libvirt/hooks/qemu.d/windows/release/end/revert.sh
+++ scripts/etc/libvirt/hooks/qemu.d/windows/release/end/revert.sh
@@ -4,6 +4,11 @@ set -x
 # Load variables
 source "/etc/libvirt/hooks/kvm.conf"
 
+# Unload all the vfio modules
+modprobe -r vfio_pci
+modprobe -r vfio_iommu_type1
+modprobe -r vfio
+
 # Rebind GPU
 virsh nodedev-reattach $VIRSH_GPU_VIDEO
 virsh nodedev-reattach $VIRSH_GPU_AUDIO
@@ -11,14 +16,19 @@ virsh nodedev-reattach $VIRSH_GPU_AUDIO
 rtcwake -m mem -s 5
 
 # load AMD
-modprobe amdgpu
-modprobe drm_kms_helper
-modprobe drm
-modprobe snd_hda_intel
+modprobe  amdgpu
+modprobe  gpu_sched
+modprobe  ttm
+modprobe  drm_kms_helper
+modprobe  i2c_algo_bit
+modprobe  drm
+modprobe  snd_hda_intel
 
 systemctl set-property --runtime -- user.slice AllowedCPUs=0-11
 systemctl set-property --runtime -- system.slice AllowedCPUs=0-11
 systemctl set-property --runtime -- init.scope AllowedCPUs=0-11
+
+cpupower frequency-set -g ondemand
 
 # Restart Display service
 systemctl start sddm.service
